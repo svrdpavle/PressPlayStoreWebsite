ab0e9f7ea3cb3f81a927bc6db429a8863c0f8875
Pavle <pavlesvrdlin@outlook.com>
2022-01-27 11:12:51 +0000

    address entity created

--- src/main/java/com/example/press_play_store_website/controllers/TestController.java
+++ src/main/java/com/example/press_play_store_website/controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.example.press_play_store_website.controllers;
 
 import com.example.press_play_store_website.repositories.ActorRepository;
+import com.example.press_play_store_website.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,16 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    private final ActorRepository actorRepository;
+    private final AddressRepository addressRepository;
 
     @Autowired
-    public TestController(ActorRepository actorRepository ) {
-        this.actorRepository = actorRepository;
+    public TestController(AddressRepository addressRepository ) {
+        this.addressRepository = addressRepository;
     }
 
     @GetMapping("/test")
     public String getAllFilms(Model model) {
-        model.addAttribute("actor", actorRepository.findAll());
+        model.addAttribute("address", addressRepository.findAll());
         return "test";
     }
 }
---
+++ src/main/java/com/example/press_play_store_website/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.example.press_play_store_website.repositories;
+
+import com.example.press_play_store_website.entities.AddressEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<AddressEntity, Integer> {
+}
