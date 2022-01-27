a61219b6d750b97622e753cb40d2392c04a6295b
Pavle <pavlesvrdlin@outlook.com>
2022-01-27 15:20:30 +0000

    functionality added to film catalogue page so that you can view more details for each film on another page

--- src/main/java/com/example/press_play_store_website/controllers/WebsiteController.java
+++ src/main/java/com/example/press_play_store_website/controllers/WebsiteController.java
@@ -1,19 +1,20 @@
 package com.example.press_play_store_website.controllers;
 
+import com.example.press_play_store_website.entities.views.FilmListEntity;
 import com.example.press_play_store_website.repositories.FilmListRepository;
-import com.example.press_play_store_website.repositories.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class FilmController {
+public class WebsiteController {
     private final FilmListRepository filmListRepository;
     //private final FilmService filmService = new FilmService();
 
     @Autowired
-    public FilmController(FilmListRepository filmListRepository) {
+    public WebsiteController(FilmListRepository filmListRepository) {
         this.filmListRepository = filmListRepository;
     }
 
@@ -21,6 +22,14 @@ public class FilmController {
     public String getAllFilms(Model model) {
         model.addAttribute("film", filmListRepository.findAll());
         return "film";
+    }
+
+    @GetMapping("/view-film/{id}")
+    public String viewFilm(@PathVariable("id") Integer id, Model model) {
+        FilmListEntity filmListEntity = filmListRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Film ID: " + id));
+        model.addAttribute("film", filmListEntity);
+        return "view-film";
     }
 
     @GetMapping("/about")
